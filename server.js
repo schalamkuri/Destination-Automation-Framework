a3d6aba6fa8ca4942ff009190b46a8f10852d0da
Saiprathik Chalamkuri <89554555+schalamkuri@users.noreply.github.com>
2022-01-27 02:43:07 -0600

    Update server.js

--- server.js
+++ server.js
@@ -13,53 +13,10 @@ app.get('/cors', async (req, res) => {
   //const data = await response.json();
   console.log(response)
   console.log("hamcho");
-  /*
-  var ham = {
-    method: 'get',
-    url: 'https://maps.googleapis.com/maps/api/directions/json?origin=2300+Nueces+Street+Austin&destination=101+E+21st+St+Austin&mode=walking&key=AIzaSyA4_UkJU3pP0Ch6HrIgH3h6T1jPrS9hAAQ',
-    //url: "https://maps.googleapis.com/maps/api/directions/json?origin="+lat+","+long+"&destination="+apiFormat+"&arrival_time="+seconds+"&mode="+mode+"&key=AIzaSyA4_UkJU3pP0Ch6HrIgH3h6T1jPrS9hAAQ",
-    headers: { }
-  };
-  
-
-  axios(ham)
-  .then(function (response) {
-    
-    var routes = response.data.routes[0];
-    var legs = routes.legs;
-    //console.log(response.data);
-    res.send(response.data);
-    //res.send({ "msg": "This has CORS enabled 🎈" })
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-  */
   })
 
 app.listen(8080, () => {
     console.log('listening on port 8080')
 })
-
-/*
-var ham = {
-      method: 'get',
-      url: 'https://maps.googleapis.com/maps/api/directions/json?origin=2300+Nueces+Street+Austin&destination=101+E+21st+St+Austin&mode=walking&key=AIzaSyA4_UkJU3pP0Ch6HrIgH3h6T1jPrS9hAAQ',
-      //url: "https://maps.googleapis.com/maps/api/directions/json?origin="+lat+","+long+"&destination="+apiFormat+"&arrival_time="+seconds+"&mode="+mode+"&key=AIzaSyA4_UkJU3pP0Ch6HrIgH3h6T1jPrS9hAAQ",
-      headers: { }
-    };
-    
-    axios(ham)
-    .then(function (response) {
-      
-      var routes = response.data.routes[0];
-      var legs = routes.legs;
-      console.log(legs);
-      res.send(legs);
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
-*/
 
 
